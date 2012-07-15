76c2a4944fa28cf01850cbf54609b4490361aa2a
Robert Wilson <1106748W@student.gla.ac.uk>
2012-07-15 21:07:44 +0100

    previous commit interrupted

---
+++ AdaptCrawler/AdaptCrawler.java
@@ -0,0 +1,288 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+
+/**
+ * This is the main class for a web crawler program for Emerge Adapt
+ * 
+ * @author Robert Wilson
+ *
+ */
+public class AdaptCrawler {
+
+	/** URL objects as instance variables */
+	private static URL testURL, siteURL;
+
+	/** Buffered reader to read incoming HTML */
+	private static BufferedReader reader;
+
+	/** String reader to read line of HTML as String */
+	private static StringReader aReader;
+
+	/** String representing page title */
+	private static String title;
+
+	/** integer representing the HTTP status code */
+	private static int stCode;
+
+	/** List of Strings representing anchor links */
+	private static List<String> links;
+
+	/** StringBuilder for use with CVS file */
+	private static StringBuilder CvsString;
+
+	/**
+	 * This is the main method for the web crawler which checks for
+	 * a URL entered at the command line and uses either this or default
+	 * URL to pass to further methods for processing
+	 * 
+	 * @param args - the arguments for the command line
+	 * 
+	 * @throws IOException 
+	 * @throws BadLocationException 
+	 */
+	public static void main(String[] args) throws IOException,
+			BadLocationException {
+
+		// String representing the URL
+		String input = "";
+
+		// Check if argument added at command line
+		if (args.length >= 1) {
+			input = args[0];
+		}
+
+		// If no argument at command line use default
+		else {
+			input = "http://www.abelsoul.co.uk/";
+			System.out.println("\nNo argument entered so default of " + input
+					+ " used: \n");
+		}
+
+		// create String builder object for CVS file
+		CvsString = new StringBuilder();
+		CvsString.append(input);
+		CvsString.append(",");
+		System.out.println("URL: " + input);
+
+		// call methods to process URL and retrieve title - throws exceptions
+		processURL(input);
+		parsePage(input);
+
+		// pass to method to create CVS file
+		generateCsvFile(CvsString);
+		System.out.println("cvs = " + CvsString);
+	}
+
+	/**
+	 * This method processes the URL and fetches the HTML
+	 * 
+	 * @param input - a String representing the URL
+	 * 
+	 * @throws IOException
+	 * @throws BadLocationException
+	 */
+	public static void processURL(String input) throws IOException,
+			BadLocationException {
+
+		// create array list of strings to represent page anchor links
+		links = new ArrayList<String>();
+
+		// add original URL to List
+		links.add(input);
+
+		// input test URL and read from file input stream
+		try {
+
+			testURL = new URL(input);
+			reader = new BufferedReader(new InputStreamReader(
+					testURL.openStream()));
+
+			// String variable to hold the returned content
+			String line = "";
+
+			// print content to console until no new lines of content
+			while ((line = reader.readLine()) != null) {
+				// System.out.println(line);
+
+				// pass each line to method to parse anchor tags for links to
+				// other pages
+				HtmlParse(line);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			System.out.println("Exception thrown");
+		}
+
+		// // Append URL to CVS StringBuilder //TODO
+		// CvsString.append(input);
+		// CvsString.append(",");
+
+		HttpURLConnection(testURL.toString());
+		// parsePage();
+	}
+
+	/**
+	 * This method retrieves the title of the web page
+	 * 
+	 * @throws BadLocationException 
+	 * @throws IOException 
+	 */
+	public static void parsePage(String pageURL) throws IOException,
+			BadLocationException {
+
+		// create new HTML editor kit object
+		HTMLEditorKit kit = new HTMLEditorKit();
+		HTMLDocument doc = (HTMLDocument) kit.createDefaultDocument();
+		doc.putProperty("IgnoreCharsetDirective", Boolean.TRUE);
+
+		// create new input reader
+		Reader HTMLReader = new InputStreamReader(testURL.openConnection()
+				.getInputStream());
+		kit.read(HTMLReader, doc, 0);
+
+		// retrieve title and convert to String
+		title = (String) doc.getProperty(Document.TitleProperty);
+		System.out.println("title: " + title);
+
+		// add to CSV file
+		CvsString.append(title);
+		CvsString.append(",");
+
+	}
+
+	// protected static void HttpURLRetrieval(String URL) throws IOException {
+	//
+	// }
+
+	/**
+	 * This method extracts the the HTTP status code as an integer and the URL
+	 * 
+	 * @throws IOException 
+	 */
+	protected static void HttpURLConnection(String URL) throws IOException {
+
+		URL pageURL = new URL(URL);
+
+		HttpURLConnection connection = (HttpURLConnection) pageURL
+				.openConnection();
+		stCode = connection.getResponseCode();
+		System.out.println("HTTP Status code: " + stCode);
+
+		// append to CVS string
+		CvsString.append(stCode);
+		CvsString.append("\n");
+
+		// retrieve URL
+		siteURL = connection.getURL();
+		System.out.println(siteURL + " = URL");
+
+		CvsString.append(siteURL);
+		CvsString.append(",");
+	}
+
+	/**
+	 * This method adds an HTML parser to make more sense of the content
+	 * and extract anchor tags
+	 * 
+	 * Swing HTML parser is apparently not thread safe and
+	 * if used in a heavily threaded environment crashing can occur
+	 * This program only runs one thread at a time
+	 *
+	 * @param line - a String representing a line of HTML
+	 * @throws IOException 
+	 */
+	public static void HtmlParse(String line) throws IOException {
+
+		// create new string reader object
+		aReader = new StringReader(line);
+
+		// create HTML parser object
+		HTMLEditorKit.Parser parser = new ParserDelegator();
+
+		// // create array list of strings to represent links //TODO remove if
+		// not needed
+		// final List<String> links = new ArrayList<String>();
+		//
+		// // add original URL to List
+		// links.add(testURL.toString());
+
+		// parse A anchor tags whilst handling start tag
+		parser.parse(aReader, new HTMLEditorKit.ParserCallback() {
+			// method to handle start tags
+			public void handleStartTag(HTML.Tag t, MutableAttributeSet a,
+					int pos) {
+				// check if A tag
+				if (t == HTML.Tag.A) {
+					Object link = a.getAttribute(HTML.Attribute.HREF);
+					if (link != null) {
+						links.add(String.valueOf(link));
+						// System.out.println(link);
+
+						// cast to string for passing to method to get title and
+						// status
+						String pageURL = link.toString();
+						try {
+							// CvsString.append(String.valueOf(link));
+							parsePage(pageURL); // Title - To print URL, HTML
+												// page title, and HTTP status
+							HttpURLConnection(pageURL); // Status
+							// pause for half a second between pages
+							Thread.sleep(500);
+
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (BadLocationException e) {
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}, true);
+		aReader.close();
+		// System.out.println(links);
+	}
+
+	/**
+	 * This method generates a CSV file containing URL, HTML page title and HTTP status code
+	 * 
+	 * @throws IOException
+	 */
+	private static void generateCsvFile(StringBuilder c) throws IOException {
+
+		try {
+
+			// create new file writer
+			FileWriter writer = new FileWriter("CVS.txt");
+
+			writer.append(c);
+
+			writer.flush();
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// TODO Code to perform JUnit tests
+
+}
---
+++ AdaptCrawler/test/AdaptCrawlerTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class AdaptCrawlerTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParsePage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHttpURLConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHtmlParse() {
+		fail("Not yet implemented");
+	}
+
+}
